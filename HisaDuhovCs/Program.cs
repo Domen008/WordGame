0368962a0e91b822655b538ab6e1d2063779e890
Domen Koder <74912929+Domen008@users.noreply.github.com>
2021-02-05 09:07:34 +0100

    Add files via upload
    Firs few lines

---
+++ HisaDuhovCs/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HisaDuhovCs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        #ScaryHouse
+         bool brave = true
+         int points = 0
+
+        }
+    }
+}
